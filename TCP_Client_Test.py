174a3c7084d284ea455d4d0ec149c2e087f59d9e
홍선 박 <dev.hspark@gmail.com>
2022-07-26 12:18:19 +0900

    Global Constance 변수 추가

--- TCP_Client_Test.py
+++ TCP_Client_Test.py
@@ -1,14 +1,14 @@
 import socket
 import struct
 
+import GlobalConst
 import CRC16
 
 #client_socket = socket.socket()
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
-    client_socket.connect(("aqss.co.kr", 6340))
-    #client_socket.connect(("115.68.114.89", 6340))
+    client_socket.connect((GlobalConst.Server_Address, GlobalConst.Server_Port))
 
     data_header = struct.pack('BBH', 0x02, 0x00, 50)
     data = struct.pack('<H12f', 1, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 0.0, 1.0, 2.0, 3.0, 5.0, 6.0)
